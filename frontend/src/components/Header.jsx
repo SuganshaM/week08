a180086c6bcfa16f5065c9b2546976be799fe2c1
SuganshaM <sugansha01@gmail.com>
2026-09-18 11:28:08 +1000

    Update header text to demonstrate automated deployment

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
             fontWeight: 600,
           }}
         >
-          KoalaTech University
+          KoalaTech University v2 for 9.3C
         </Typography>
 
         <Box
